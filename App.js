41eeba62614be3dcfc285ead1faf637db4dccfb0
Momo <haphuocdang@gmail.com>
2022-07-25 16:22:41 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+export default App;
