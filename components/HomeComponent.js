289f3887529eb50d12af72955acea8ca56fe4def
Momo <haphuocdang@gmail.com>
2022-07-29 09:47:57 +0700

    complete 1.7 prepare for assignment 1

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,62 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, ScrollView, Text } from "react-native";
+import { Card, Image } from "react-native-elements";
+
+/* Shared Folder */
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
+class RenderItem extends Component {
+  render() {
+    const item = this.props.item;
+    if (item != null) {
+      return (
+        <Card>
+          <Image
+            source={require("./images/uthappizza.png")}
+            style={{
+              width: "100%",
+              height: 100,
+              flexGrow: 1,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Card.FeaturedTitle>{item.name}</Card.FeaturedTitle>
+            <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>
+          </Image>
+          <Text style={{ margin: 10 }}>{item.description}</Text>
+        </Card>
+      );
+    }
+    return <View />;
+  }
+}
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+    };
+  }
   render() {
+    const dish = this.state.dishes.filter((dish) => dish.featured === true)[0];
+    const promotions = this.state.dishes.filter(
+      (promotions) => promotions.featured === true
+    )[0];
+    const leaders = this.state.dishes.filter(
+      (leaders) => leaders.featured === true
+    )[0];
     return (
-      <View><Text>Home Component</Text></View>
+      <ScrollView>
+        <RenderItem item={dish} />
+        <RenderItem item={promotions} />
+        <RenderItem item={leaders} />
+      </ScrollView>
     );
   }
 }
