5c5f1ef0ec41086e0ce03ac0cf73b5a678b08c0c
Momo <haphuocdang@gmail.com>
2022-08-01 10:09:31 +0700

    assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import Dishdetail from './DishdetailComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-
-import Menu from './MenuComponent';
 import Home from './HomeComponent';
-import Dishdetail from './DishdetailComponent';
-
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 function HomeNavigatorScreen() {
   const HomeNavigator = createStackNavigator();
   return (
@@ -21,7 +22,31 @@ function HomeNavigatorScreen() {
     </HomeNavigator.Navigator>
   );
 }
-
+function MainNavigatorScreen() {
+  const MainNavigator = createDrawerNavigator();
+  return (
+    <MainNavigator.Navigator initialRouteName='Home'>
+      <MainNavigator.Screen name='Home' component={HomeNavigatorScreen}
+        options={{
+          headerShown: false
+        }} />
+      <MainNavigator.Screen name='About' component={AboutNavigatorScreen}
+        options={{
+          headerShown: false,
+          title: 'About Us'
+        }} />
+      <MainNavigator.Screen name='Menu' component={MenuNavigatorScreen}
+        options={{
+          headerShown: false
+        }} />
+      <MainNavigator.Screen name='Contact' component={ContactNavigatorScreen}
+        options={{
+          headerShown: false,
+          title: 'Contact Us'
+        }} />
+    </MainNavigator.Navigator>
+  );
+}
 function MenuNavigatorScreen() {
   const MenuNavigator = createStackNavigator();
   return (
@@ -37,24 +62,48 @@ function MenuNavigatorScreen() {
     </MenuNavigator.Navigator>
   );
 }
-
-function MainNavigatorScreen() {
-  const MainNavigator = createDrawerNavigator();
+const AboutNavigator = createStackNavigator();
+function AboutNavigatorScreen() {
   return (
-    <MainNavigator.Navigator initialRouteName='HomeScreen'>
-      <MainNavigator.Screen name='HomeScreen' component={HomeNavigatorScreen} options={{ title: 'Home', headerShown: false }} />
-      <MainNavigator.Screen name='MenuScreen' component={MenuNavigatorScreen} options={{ title: 'Menu', headerShown: false }} />
-    </MainNavigator.Navigator>
+    <AboutNavigator.Navigator initialRouteName='About'
+      screenOptions={{
+        headerStyle: { backgroundColor: '#512DA8' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { color: '#fff' }
+      }}>
+      <AboutNavigator.Screen name='About' component={About} />
+    </AboutNavigator.Navigator>
   );
 }
-
+const ContactNavigator = createStackNavigator();
+function ContactNavigatorScreen() {
+  return (
+    <ContactNavigator.Navigator initialRouteName='Contact'
+      screenOptions={{
+        headerStyle: { backgroundColor: '#512DA8' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { color: '#fff' }
+      }}>
+      <ContactNavigator.Screen name='Contact' component={Contact} />
+    </ContactNavigator.Navigator>
+  );
+}
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
+      // <Menu dishes={this.state.dishes} />
       <NavigationContainer>
-        <MainNavigatorScreen />
-      </NavigationContainer>
+      <MainNavigatorScreen  />
+    </NavigationContainer>
+      
     );
   }
 }
+
 export default Main;
